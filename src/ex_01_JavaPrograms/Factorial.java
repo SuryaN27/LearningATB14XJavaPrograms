a418c10441dd643a8b9f02e41fc35e825f199cac
SuryaN27 <surya9663567@gmail.com>
2025-10-02 17:19:22 +0530

    Fact program

---
+++ src/ex_01_JavaPrograms/Factorial.java
@@ -0,0 +1,28 @@
+package ex_01_JavaPrograms;
+import java.util.Scanner;
+
+public class Factorial {
+
+            public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the number");
+            int n = scanner.nextInt();
+            int fact = 1;
+            if (n == 0) {
+                fact = 1;
+            }
+
+            // for (int i = n; i >=1 ; i--) {
+            for (int i = 1; i <= n ; i++) {
+                fact = fact*i;
+            }
+            System.out.println(fact);
+
+
+
+
+
+
+        }
+    }
+
